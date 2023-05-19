866191b44495fd78d8b2110efd593d56cfd60beb
Jeremy23a <jeremy.andre.99@gmail.com>
2023-05-19 10:34:38 -0500

    Primer commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import { Home, About, NotFound } from "./Views";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/Views/index.tsx
@@ -0,0 +1,4 @@
+import Home from "./Home";
+import About from "./About";
+import NotFound from "./NotFound";
+export { Home, About, NotFound };
