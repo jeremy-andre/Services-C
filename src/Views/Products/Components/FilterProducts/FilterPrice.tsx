560576f05d4ab50ec2666cec0a890583accb091b
Jeremy23a <jeremy.andre.99@gmail.com>
2023-07-11 18:06:36 -0500

    (Fix-FilterPrice): New Open Add

--- src/Views/Products/Components/FilterProducts/FilterPrice.tsx
+++ src/Views/Products/Components/FilterProducts/FilterPrice.tsx
@@ -32,34 +32,36 @@ const FilterPrice: React.FC<FilterPriceProps> = ({ min, max }) => {
       <Button onClick={onToggle} rightIcon={<BiDownArrow />}>
         Precio
       </Button>
-      <Fade in={isOpen}>
-        <Flex
-          direction="column"
-          p="2rem"
-          w="25rem"
-          mt="4"
-          bg={useColorModeValue("#dedede", "#1f1f1f")}
-          boxShadow="0px 0px 5px rgba(0, 0, 0, 0.3)"
-          rounded="md"
-          position="absolute"
-        >
-          <RangeSlider
-            onChange={handleChange}
-            defaultValue={range}
-            min={min}
-            max={max}
+      {isOpen && (
+        <Fade in={true}>
+          <Flex
+            direction="column"
+            p="2rem"
+            w="25rem"
+            mt="4"
+            bg={useColorModeValue("#dedede", "#1f1f1f")}
+            boxShadow="0px 0px 5px rgba(0, 0, 0, 0.3)"
+            rounded="md"
+            position="absolute"
           >
-            <RangeSliderTrack>
-              <RangeSliderFilledTrack bg="#cc5823" />
-            </RangeSliderTrack>
-            <RangeSliderThumb index={0} />
-            <RangeSliderThumb index={1} />
-          </RangeSlider>
-          <Text mt="2" textAlign="center">
-            S/.{range[0]} - S/.{range[1]}
-          </Text>
-        </Flex>
-      </Fade>
+            <RangeSlider
+              onChange={handleChange}
+              defaultValue={range}
+              min={min}
+              max={max}
+            >
+              <RangeSliderTrack>
+                <RangeSliderFilledTrack bg="#cc5823" />
+              </RangeSliderTrack>
+              <RangeSliderThumb index={0} />
+              <RangeSliderThumb index={1} />
+            </RangeSlider>
+            <Text mt="2" textAlign="center">
+              S/.{range[0]} - S/.{range[1]}
+            </Text>
+          </Flex>
+        </Fade>
+      )}
     </Flex>
   );
 };
